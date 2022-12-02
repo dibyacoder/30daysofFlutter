a551c6f91765f236f00ef953f459d5c0d8c1e71c
Dibyacoder <b421020@iiit-bh.ac.in>
2022-12-02 16:30:54 +0530

    day12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 import 'dart:ffi';
 import 'dart:convert';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:myfirst_project/utils/routes.dart';
@@ -28,7 +29,7 @@ class _homepageState extends State<homepage> {
   }
 
   loaddata() async {
-    await Future.delayed(Duration(seconds: 5));
+    await Future.delayed(Duration(seconds: 3));
     var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
     var decodedData = json.decode(catalogJson);
     var productData = decodedData["products"];
@@ -42,18 +43,48 @@ class _homepageState extends State<homepage> {
     return Scaffold(
       appBar: AppBar(title: Text("Catalog App")),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: (catalogModel.items != null && catalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: catalogModel.items.length,
-                itemBuilder: (context, index) {
-                  return Itemwidget(
-                    item: catalogModel.items[index],
-                  );
-                })
+        padding: const EdgeInsets.all(0.0),
+        child: (catalogModel.items != null && catalogModel.items!.isNotEmpty)
+            ? Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 12),
+                    itemCount: catalogModel.items!.length,
+                    itemBuilder: (context, index) {
+                      final item = catalogModel.items![index];
+                      return Card(
+                          clipBehavior: Clip.antiAlias,
+                          shadowColor: Color.fromARGB(255, 101, 101, 185),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: GridTile(
+                              header: Container(
+                                child: Text(item.name,
+                                    style: TextStyle(color: Colors.white)),
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 19, 79, 122),
+                                ),
+                              ),
+                              child: Image.network(item.image),
+                              footer: Container(
+                                child: Text(item.price.toString(),
+                                    style: TextStyle(color: Colors.white)),
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                              ),
+                            ),
+                          ));
+                    }),
+              )
             : Center(
                 child: CircularProgressIndicator(
-                  color: Colors.black,
+                  color: Color.fromARGB(255, 23, 42, 161),
                 ),
               ),
       ),
